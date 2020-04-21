6d4300e0d74da59e05c0cfb47a441571bad3ad31
JoaoResende <jvr1998@hotmail.com>
2020-04-21 17:11:10 -0300

    listas

---
+++ Atividades/lista3/lista3q4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int maior(int a, int b){
+    int resultadoma;
+    if (a>b){
+        resultadoma = a;
+    }
+    else{
+        resultadoma = b;
+    }
+    return(resultadoma);
+}
+
+int menor(int a, int b){
+    int resultadome;
+    if (a<b){
+        resultadome = a;
+    }
+    else{
+        resultadome = b;
+    }
+    return(resultadome);
+}
+
+int ordena(int *vetor){
+    int resultado;
+
+    return(resultado);
+}
+
+    return 0;
+}
